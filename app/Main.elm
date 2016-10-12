c3936686b2c13ae6e7524e441057c93b7fe0416d
Rogerio Chaves <rogeriocfj+github@gmail.com>
2016-10-12 00:11:32 -0300

    anota tipos

--- app/Main.elm
+++ app/Main.elm
@@ -1,9 +1,12 @@
 import Html exposing (Html, button, div, text)
 import Html.App as App
 
+main : Program Never
 main = App.beginnerProgram { model = 0, view = view, update = update }
 
+update : a -> a
 update a = a
 
+view : a -> Html b
 view model =
     text "hello world"
