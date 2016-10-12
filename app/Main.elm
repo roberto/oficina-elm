e643d523b5b69058b46bb56b8476245355ed948a
Rogerio Chaves <rogeriocfj+github@gmail.com>
2016-10-12 18:28:08 -0300

    pega o nome do pokémon de uma api

--- app/Main.elm
+++ app/Main.elm
@@ -4,6 +4,9 @@ import Html exposing (Html, button, h2, div, img, text)
 import Html.App as App
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (src)
+import Task
+import Json.Decode as Json
+import Http
 
 
 main : Program Never
@@ -29,6 +32,8 @@ init =
 
 type Msg
     = GetPokemon
+    | FetchSucceed String
+    | FetchFail Http.Error
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -36,10 +41,21 @@ update msg model =
     case msg of
         GetPokemon ->
             ( { model
-                | image = "http://www.pokestadium.com/assets/img/sprites/1.png"
+                | name = "Loading..."
+                , image = "http://www.pokestadium.com/assets/img/sprites/1.png"
+              }
+            , getPokemon 1
+            )
+
+        FetchSucceed name ->
+            ( { model
+                | name = name
               }
             , Cmd.none
             )
+
+        FetchFail image ->
+            init
 
 
 view : Model -> Html Msg
@@ -49,3 +65,17 @@ view model =
         , h2 [] [ text model.name ]
         , img [ src model.image ] []
         ]
+
+
+getPokemon : Int -> Cmd Msg
+getPokemon pokemonId =
+    let
+        url =
+            "http://pokeapi.co/api/v2/pokemon/" ++ (toString pokemonId) ++ "/"
+    in
+        Task.perform FetchFail FetchSucceed (Http.get decodePokemonName url)
+
+
+decodePokemonName : Json.Decoder String
+decodePokemonName =
+    Json.at [ "name" ] Json.string
