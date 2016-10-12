7ed7261f5718c38720abcff5048465067674665f
Rogerio Chaves <rogeriocfj+github@gmail.com>
2016-10-12 18:05:50 -0300

    adiciona examplo de pegar imagens de pokemons

--- app/Main.elm
+++ app/Main.elm
@@ -1,53 +1,47 @@
 module Main exposing (..)
 
-import Html exposing (Html, button, div, text, input)
+import Html exposing (Html, button, h2, div, img, text)
 import Html.App as App
-import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (value)
-import String
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (src)
 
 
 main : Program Never
 main =
-    App.beginnerProgram { model = 0, view = view, update = update }
-
-
-type Msg
-    = Increment
-    | Decrement
-    | Change String
+    App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        }
 
 
 type alias Model =
-    Int
+    { name : String
+    , image : String
+    }
 
 
-update : Msg -> Model -> Model
+init : ( Model, Cmd Msg )
+init =
+    ( Model "MissingNo" "http://bit.ly/missingno-sprite", Cmd.none )
+
+
+type Msg
+    = GetPokemon
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            model + 1
-
-        Decrement ->
-            model - 1
-
-        Change strValue ->
-            case (String.toInt strValue) of
-                Ok value ->
-                    value
-
-                Err _ ->
-                    model
+        GetPokemon ->
+            ( model, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        , div []
-            [ text "Mudar para:"
-            , input [ value (toString model), onInput Change ] []
-            ]
+        [ button [ onClick GetPokemon ] [ text "Get more Pokémons!" ]
+        , h2 [] [ text model.name ]
+        , img [ src model.image ] []
         ]
