ae2be4a9784da401b6dab16951925b38f4380632
Rogerio Chaves <rogeriocfj+github@gmail.com>
2016-10-12 18:51:17 -0300

    atualiza a data a cada tick do relogio

--- app/Main.elm
+++ app/Main.elm
@@ -8,6 +8,8 @@ import Task
 import Json.Decode as Json
 import Http
 import Random
+import Time exposing (Time, second)
+import Date exposing (Date, fromTime)
 
 
 main : Program Never
@@ -16,19 +18,30 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Time.every second Tick
 
 
 type alias Model =
     { name : String
     , image : String
+    , date : Date
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "MissingNo" "http://bit.ly/missingno-sprite", Cmd.none )
+    ( { name = "MissingNo"
+      , image = "http://bit.ly/missingno-sprite"
+      , date = fromTime 0
+      }
+    , Cmd.none
+    )
 
 
 type Msg
@@ -36,6 +49,7 @@ type Msg
     | FetchPokemon Int
     | FetchSucceed String
     | FetchFail Http.Error
+    | Tick Time
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -62,11 +76,17 @@ update msg model =
         FetchFail image ->
             init
 
+        Tick time ->
+            ( { model | date = fromTime time }, Cmd.none )
+
 
 view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick GetRandomPokemon ] [ text "Get more Pokémons!" ]
+        , model.date
+            |> toString
+            |> text
         , h2 [] [ text model.name ]
         , img [ src model.image ] []
         ]
