bc464ca7d3be7d0c344723e9a38bf335aad1282a
Rogerio Chaves <rogeriocfj+github@gmail.com>
2016-10-12 08:34:11 -0300

    formata com elm format

--- app/Main.elm
+++ app/Main.elm
@@ -1,28 +1,38 @@
+module Main exposing (..)
+
 import Html exposing (Html, button, div, text)
 import Html.App as App
 import Html.Events exposing (onClick)
 
+
 main : Program Never
 main =
-  App.beginnerProgram { model = 0, view = view, update = update }
+    App.beginnerProgram { model = 0, view = view, update = update }
 
-type Msg = Increment | Decrement
 
-type alias Model = Int
+type Msg
+    = Increment
+    | Decrement
+
+
+type alias Model =
+    Int
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model + 1
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model - 1
+
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
